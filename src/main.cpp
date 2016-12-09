83f5f661fbffd102ad2d08e806dd8a59a1a06eb6
Mariusz Sikora <msikora87@gmail.com>
2016-12-09 19:29:20 +0100

    work in progress

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <avr/io.h>
+#include "Nrf24l01.h"
 
 int main() {
+
+    nrf24l01_driver::Nrf24l01 nrf(
+            0,
+            0);
+
+    while(1)
+    {
+        nrf.FlushRx();
+        nrf.FlushTx();
+    }
 
     return 0;
 }
