a317ecaa3e102f2955b0098a1ce7980efe7aeb18
Mariusz Sikora <msikora87@gmail.com>
2016-12-21 13:29:30 +0100

    Preparing demo for Rpi

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,22 @@
-#define F_CPU 7372800UL 
-#include "UartAvr.h"
-#include "Console.h"
-#include "IWait.h"
-#include "GpioAvrDriver.h"
-#include "OneWireDriver.h"
-#include "Ds18b20.h"
-#include <avr/io.h>
-#include <util/delay.h>
+#if defined(TARGET_PLATFORM_AVR)
+#   define F_CPU 7372800UL
+#   include "GpioAvrDriver.h"
+#   include "UartAvr.h"
+#   include <avr/io.h>
+#   include <util/delay.h>
+#   include "ConsoleAvr.h"
+#elif defined(TARGET_PLATFORM_RASPBERRYPI)
+#   include "GpioRpi.h"
+#   include "ConsoleRpi.h"
+#   include <unistd.h>
+#endif
 
+#include "OneWireDriver.h"
+#include "IWait.h"
+
+#include "Ds18b20.h"
+
+#if defined(TARGET_PLATFORM_AVR)
 class AvrWait : public iwait::IWait {
 
 public:
@@ -42,15 +51,42 @@ public:
             _delay_ms(1);
     };
 };
+#elif defined(TARGET_PLATFORM_RASPBERRYPI)
+class RpiWait : public iwait::IWait {
+
+public:
+    virtual void wait_us(uint16_t time) {
+        usleep(time);    
+    }
+
+    virtual void wait_ms(uint16_t time) {
+        usleep(time * 1000);
+    }
+};
+#endif
 
 
 int main() {
+#if defined(TARGET_PLATFORM_AVR)
     DDRC = 0xFF;
     PORTC = 0xFF;
     PINC = 0xFF;
-    AvrWait avr_wait;
-    uart_avr::UartAvr uart(uart_driver::BAUD_19200);
+#endif
 
+#if defined(TARGET_PLATFORM_AVR)
+#   pragma message ("Platform AVR")
+#elif defined(TARGET_PLATFORM_RASPBERRYPI)
+#   pragma message ("Platform Raspberry pi")
+#endif
+
+#if defined(TARGET_PLATFORM_AVR)
+    AvrWait wait;
+    uart_avr::UartAvr uart(uart_driver::BAUD_19200);
+#elif defined(TARGET_PLATFORM_RASPBERRYPI)
+    RpiWait wait;
+#endif
+
+#if defined(TARGET_PLATFORM_AVR)
     gpio_avr_driver::GpioAvr one_wire_gpio(
             DDRC,
             PORTC,
@@ -59,40 +95,52 @@ int main() {
             gpio_driver::GPIO_INPUT,
             gpio_driver::GPIO_NO_PULL);
 
+#elif defined(TARGET_PLATFORM_RASPBERRYPI)
+    gpio_rpi::GpioRpi one_wire_gpio(
+            17,
+            gpio_driver::GPIO_INPUT,
+            gpio_driver::GPIO_NO_PULL);
+#endif
+
     one_wire_driver::OneWireDriver one_wire(
             one_wire_gpio,
-            avr_wait);
+            wait);
 
     ds18b20::Ds18b20 termo(
             one_wire,
-            avr_wait);
+            wait);
 
-
-    console::Console serial(uart);
+#if defined(TARGET_PLATFORM_AVR)
+    console_avr::ConsoleAvr console(uart);
+#elif defined(TARGET_PLATFORM_RASPBERRYPI)
+    console_rpi::ConsoleRpi console;
+#endif
 
     ds18b20::Temp temp;
     temp.value = 64;
 
-    for(int i = 0; i < 100; i ++)
-        serial.newline();
+    for(int i = 0; i < 5; i ++)
+        console.newline();
+
+    console.print("----Start----").newline();
 
 
     while(1) {
-        avr_wait.wait_ms(2000);
+        wait.wait_ms(2000);
 
         temp = termo.GetTemp();
-        serial.print("Temp=").print((int8_t)(temp.value >> 4))
+        console.print("Temp=").print((int8_t)(temp.value >> 4))
             .print(".")
             .print((temp.value & 0x0F) * 6)
             .newline();
         for (int i = 0; i < 16; i++) {
             if (temp.value & (1 << (15 - i)))
-                serial.print("1");
+                console.print("1");
             else
-                serial.print("0");
+                console.print("0");
         }
 
-        serial.newline();
+        console.newline();
     }
 
     return 0;
